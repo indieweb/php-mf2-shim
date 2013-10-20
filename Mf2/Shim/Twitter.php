4a2211465391311874870689cf245e1903f01858
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-10-20 17:29:40 +0000

    Moved a bunch of stuff around, made things more consistent, updated namespaces, autoloading, tests

---
+++ Mf2/Shim/Twitter.php
@@ -0,0 +1,87 @@
+<?php
+namespace Mf2\Shim;
+
+use Mf2;
+use DateTime;
+use DOMElement;
+use Exception;
+
+function parseTwitter($html, $url=null) {
+	$parser = new Twitter($html, $url, false);
+	return array_merge($parser->parseRelsAndAlternates(), $parser->parse());
+}
+
+class Twitter extends Mf2\Parser {
+	public function parseTweet(DOMElement $el, $parseReplies=true) {
+		$tweetTextEl = $this->query('.//p' . Mf2\xpcs('tweet-text'), $el)->item(0);
+
+		$authorNameEl = $this->query('.//*' . Mf2\xpcs('fullname'), $el)->item(0);
+		$authorNickEl = $this->query('.//*' . Mf2\xpcs('username'), $el)->item(0);
+		$authorPhotoEl = $this->query('.//*' . Mf2\xpcs('avatar'), $el)->item(0);
+
+		$publishedEl = $this->query('.//*' . Mf2\xpcs('_timestamp'), $el)->item(0);
+		$publishedTimestamp = $publishedEl->getAttribute('data-time');
+		try {
+			$publishedDateTime = DateTime::createFromFormat('U', $publishedTimestamp)->format(DateTime::W3C);
+		} catch (Exception $e) {
+			$publishedDateTime = '';
+		}
+
+		$urlEl = $this->query('.//*' . Mf2\xpcs('tweet-timestamp'), $el)->item(0);
+		
+		$htmlTweetContent = '';
+		foreach ($tweetTextEl->childNodes as $node) {
+			$htmlTweetContent .= $node->C14N();
+		}
+		
+		$tweet = array(
+			'type' => array('h-entry'),
+			'properties' => array(
+				'uid' => array(),
+				'name' => array($tweetTextEl->nodeValue),
+				'content' => array(array(
+					'value' => $tweetTextEl->nodeValue,
+					'html' =>  $htmlTweetContent
+				)),
+				'summary' => array($tweetTextEl->nodeValue),
+				'url' => array($urlEl->getAttribute('href')),
+				'published' => array($publishedDateTime),
+				'author' => array(
+						array(
+						'type' => array('h-card'),
+						'properties' => array(
+							'uid' => array(),
+							'name' => array($authorNameEl->nodeValue),
+							'nickname' => array($authorNickEl->nodeValue),
+							'photo' => array($authorPhotoEl->getAttribute('src')),
+							'url' => array('https://twitter.com/' . $authorNickEl->nodeValue)
+						)
+					)
+				)
+			)
+		);
+		
+		if ($parseReplies) {
+			foreach ($this->query('//*' . Mf2\xpcs('permalink-replies') . '//*' . Mf2\xpcs('tweet')) as $reply) {
+				$tweet['properties']['comment'][] = $this->parseTweet($reply, false);
+			}
+		}
+		
+		return $tweet;
+	}
+	
+	/**
+   * Parse
+   * 
+   * @return array
+   */
+  public function parse() {
+    $items = array();
+
+    foreach($this->query('//div' . Mf2\xpcs('tweet', 'permalink-tweet')) as $node) {
+      $items[] = $this->parseTweet($node);
+    }
+
+    return array('items' => array_values(array_filter($items)));
+  }
+}
---
+++ tests/Mf2/TwitterTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Mf2\Shim\Test;
+
+use Mf2;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Shim Test
+ */
+class TwitterTest extends PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+		date_default_timezone_set('Europe/London');
+	}
+
+	public function testParsesHEntryFromTweetPermalinkHtml() {
+		$input = file_get_contents('./tests/mf2/example-twitter.html');
+		$output = Mf2\Shim\parseTwitter($input);
+		$this->assertArrayHasKey('items', $output);
+		$this->assertCount(1, $output['items']);
+
+		return $output;
+	}
+
+	/**
+	 * @depends testParsesHEntryFromTweetPermalinkHtml
+	 * @todo what’s with the space before the closing paren? More nbsp annoyance? Fix
+	 */
+	public function testHEntryHasContent($output) {
+		$this->assertArrayHasKey('content', $output['items'][0]['properties']);
+		$this->assertEquals('Started off the week getting up at 5am, ended the week going to sleep at 5am. I am my own little timezone. (http://aaron.pk/n4Px1 )', $output['items'][0]['properties']['content'][0]['value']);
+	}
+
+	/**
+	 * @depends testParsesHEntryFromTweetPermalinkHtml
+	 */
+	public function testHEntryHasPublished($output) {
+		$this->assertArrayHasKey('published', $output['items'][0]['properties']);
+		$this->assertEquals('2013-05-13T02:30:56+00:00', $output['items'][0]['properties']['published'][0]);
+	}
+
+	/**
+	 * @depends testParsesHEntryFromTweetPermalinkHtml
+	 */
+	public function testHEntryHasAuthor($output) {
+		$this->assertArrayHasKey('author', $output['items'][0]['properties']);
+
+		$author = $output['items'][0]['properties']['author'][0];
+
+		$this->assertEquals('Aaron Parecki', $author['properties']['name'][0]);
+		$this->assertEquals('https://twitter.com/@aaronpk', $author['properties']['url'][0]);
+		$this->assertEquals('https://si0.twimg.com/profile_images/3657148842/934fb225b84b8fd3effe5ab95bb18005_normal.jpeg', $author['properties']['photo'][0]);
+	}
+
+	/**
+	 * @depends testParsesHEntryFromTweetPermalinkHtml
+	 */
+	public function testHEntryHasComments($output) {
+		$this->assertArrayHasKey('comment', $output['items'][0]['properties']);
+		$comments = $output['items'][0]['properties']['comment'];
+
+		$this->assertCount(1, $comments);
+
+		$comment = $comments[0]['properties'];
+		$author = $comment['author'][0]['properties'];
+
+		$this->assertEquals('Rachel Kalmar', $author['name'][0]);
+		$this->assertEquals('@grapealope', $author['nickname'][0]);
+	}
+}
