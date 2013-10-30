5c3ecd653b35416a8e5e8e8b51d27f1b441c8636
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-10-30 14:07:56 +0000

    Added post content parsing, updated dependencies

--- Mf2/Shim/Facebook.php
+++ Mf2/Shim/Facebook.php
@@ -12,18 +12,36 @@ function parseFacebook($html, $url=null) {
 	return array_merge(array('rels' => $rels, 'alternates' => $alternates), $parser->parse());
 }
 
+function fbTimeToIso8601($t) {
+	echo $t;
+	$dt = DateTime::createFromFormat('l, F j, Y \a\t g:ia', $t);
+	return $dt ? $dt->format(DateTime::ISO8601) : null;
+}
+
 class Facebook extends Mf2\Parser {
 	public function parsePost(DOMElement $el) {
-		/*
-		$htmlTweetContent = '';
-		foreach ($tweetTextEl->childNodes as $node) {
-			$htmlTweetContent .= $node->C14N();
-		}*/
-		
 		$authorPhoto = $this->query('.//*' . Mf2\xpcs('fbStreamPermalinkHeader') . '//*' . Mf2\xpcs('profilePic'))->item(0)->getAttribute('src');
 		$authorLink = $this->query('.//*' . Mf2\xpcs('permalinkHeaderInfo') . '/a')->item(0);
 		$authorUrl = $authorLink->getAttribute('href');
 		$authorName = trim($authorLink->textContent);
+		
+		$postLink = $this->query('.//*' . Mf2\xpcs('permalinkHeaderContentText') . '//*' . Mf2\xpcs('uiStreamSource') . '/a')->item(0);
+		// TODO: resolve once php-mf2 is updated making ->resolveUrl() public
+		$postUrl = $postLink->getAttribute('href');
+		$postPublished = fbTimeToIso8601($this->query('./abbr', $postLink)->item(0)->getAttribute('title'));
+		
+		$contentEl = $this->query('.//*' . Mf2\xpcs('userContentWrapper') . '//*' . Mf2\xpcs('userContent'))->item(0);
+		foreach ($this->query('.//a[starts-with(@onmouseover, "LinkshimAsyncLink")]') as $linkEl) {
+			$linkEl->setAttribute('href', $linkEl->textContent);
+			$linkEl->removeAttribute('onclick');
+			$linkEl->removeAttribute('onmouseover');
+			$linkEl->removeAttribute('target');
+		}
+		$contentPlaintext = $contentEl->textContent;
+		$contentHtml = '';
+		foreach ($contentEl->childNodes as $node) {
+			$contentHtml .= $node->C14N();
+		}
 		
 		$post = array(
 			'type' => array('h-entry'),
@@ -35,7 +53,10 @@ class Facebook extends Mf2\Parser {
 						'name' => array($authorName),
 						'url' => array($authorUrl)
 					)
-				)
+				),
+				'url' => array($postUrl),
+				'published' => array($postPublished),
+				'content' => array(array('value' => $contentPlaintext, 'html' => $contentHtml))
 			)
 		);
 
