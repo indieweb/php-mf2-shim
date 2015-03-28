6f17367b42956d5457b167e79d9432c6a98c2e4b
BenRoberts <ben@cs.uri.edu>
2015-03-28 08:24:25 -0400

    fix failing twitter unit test

--- tests/Mf2/FacebookTest.php
+++ tests/Mf2/FacebookTest.php
@@ -18,7 +18,7 @@ class FacebookTest extends PHPUnit_Framework_TestCase {
 		$input = file_get_contents('./tests/Mf2/example-facebook.html');
 		$output = Mf2\Shim\parseFacebook($input, 'https://www.facebook.com/barnaby.walters/posts/587281857995088');
 		
-		print_r($output);
+		//print_r($output);
 		
 		$this->assertArrayHasKey('items', $output);
 		$this->assertCount(1, $output['items']);
